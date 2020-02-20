1712bbe90223b866f165e0d58d4d7147799d8b5d
Sille Van Landschoot <info@sillevl.be>
2020-02-20 19:59:19 +0100

    add repo

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,5 +1,12 @@
 module.exports = {
   title: 'Software Engineering',
   description: 'Software Engineering course for the VIVES elektronics and ICT',
-  dest: 'dist'
+  dest: 'dist',
+  themeConfig: {
+    repo: 'sillevl/course-software-engineering',
+    sidebarDepth: 1
+  },
+  markdown: {
+    lineNumbers: true
+  }
 }
